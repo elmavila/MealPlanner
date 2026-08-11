7c7e22b8bbe2dcfaaa8294a14d7495a7cd920c68
Elma <vilaelma1@gmail.com>
2026-08-11 14:23:11 +0200

    local

--- fullstack/frontend/src/helpers/apiHelpers.tsx
+++ fullstack/frontend/src/helpers/apiHelpers.tsx
@@ -1,4 +1,5 @@
-export const ApiUrl = "https://chrispys.top/meal-server";
+// export const ApiUrl = "https://chrispys.top/meal-server";
 //https://chrispys.top/
 // export const ApiUrl = 'http://localhost:3032';
 // TODO use env variables for API url instead. OR check if prod or debug is an option in nodejs
+export const ApiUrl = 'http://localhost:3032';
